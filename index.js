1a6297a4da64b067424ee923c10dde5099646666
Benjamin Connor <bsconnor@gmail.com>
2019-12-18 18:52:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 var name = "Joe"
 var height = "74"
 
+if (name === 'Susan') {
+  console.log('true');
+} else {
+  console.log('false');
+}
 
 // Don't worry about this
 module.exports = { name, height
