4d517d507023b051e3810cddf892002b57799207
Benjamin Connor <bsconnor@gmail.com>
2019-12-18 18:54:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,14 +1,8 @@
 var name = "Joe";
 var height = "74";
-name = 'Susan';
 
-if (name === 'Susan') {
-  console.log('true');
-} else {
-  console.log('false');
-}
 
 // Don't worry about this
 module.exports = { name, height
-}
+};
 
